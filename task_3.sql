d3edf6c43d202b0261e90d8f1e2b794a80260d40
Arthur-CracksIt <davidkwadwoarthur.tech@gmail.com>
2025-07-04 20:27:56 +0000

    show tables

--- task_3.sql
+++ task_3.sql
@@ -1,2 +1,3 @@
-
-SELECT table_name FROM information_schema.tables WHERE table_schema = 'alx_book_store'
+USE alx_book_store;
+SHOW TABLES;
+-- SELECT table_name FROM information_schema.tables WHERE table_schema = 'alx_book_store'
