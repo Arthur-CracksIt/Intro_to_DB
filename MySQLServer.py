e82db7d02be3988665288b7d8f91d8720f5e6a27
Arthur-CracksIt <davidkwadwoarthur.tech@gmail.com>
2025-07-04 20:06:03 +0000

    added try and except block

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,10 +1,15 @@
 import mysql.connector
 import check
 
-connection = mysql.connector.connect(host= check.user_credentials['host'], user = check.user_credentials['username'], passwd= check.user_credentials['password'])
-
-myCursor = connection.cursor()
-query = "CREATE DATABASE IF NOT EXISTS alx_book_store"
-
-myCursor.execute(query)
-connection.commit()
+try:
+    connection = mysql.connector.connect(host= check.user_credentials['host'], 
+                                         user = check.user_credentials['username'], 
+                                         passwd= check.user_credentials['password'])
+    myCursor = connection.cursor()
+    query = "CREATE DATABASE IF NOT EXISTS %s"
+    dbName = 'alx_book_store'
+    myCursor.execute(query, dbName,)
+    connection.commit()
+    print(f"Database {dbName} created successfully!")
+except mysql.connector.Error as error:
+    print(f"Error: {error}")
