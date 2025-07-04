218c8e312c33022e54e2788fae574f3e7397d73c
Arthur-CracksIt <davidkwadwoarthur.tech@gmail.com>
2025-07-04 11:13:54 +0000

    order and orderdetails script revamped

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -5,5 +5,5 @@ USE alx_book_store;
 CREATE TABLE Books(book_id int AUTO_INCREMENT PRIMARY KEY , title varchar(130) not null, author_id FOREIGN KEY REFERENCES Authors(author_id), price not null, publication_date date);
 CREATE TABLE Authors(author_id int AUTO_INCREMENT PRIMARY KEY, author_name varchar(215) not null);
 CREATE TABLE Customers(customer_id int AUTO_INCREMENT PRIMARY KEY, customer_name VARCHAR(215) not null, email VARCHAR(215) not null UNIQUE, address TEXT);
-CREATE TABLE Orders(customer_id INT AUTO_INCREMENT PRIMARY KEY, order_date date, FOREIGN KEY(customer_id) REFERENCES Customers(customer_id));
+CREATE TABLE Orders(order_id INT, order_date date, FOREIGN KEY(customer_id) REFERENCES Customers(customer_id));
 CREATE TABLE Order_details(orderdetailid int primary key, FOREIGN KEY (order_id) REFERENCES Orders(order_id),FOREIGN KEY (book_id) REFERENCES Books(book_id), quantity double)
