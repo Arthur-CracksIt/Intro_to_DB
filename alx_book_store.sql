1f45db35a0ac4626defa003c056933b992518be3
Arthur-CracksIt <davidkwadwoarthur.tech@gmail.com>
2025-07-04 11:02:53 +0000

    create alx book store database

---
+++ alx_book_store.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE alx_book_store;
+
+USE alx_book_store;
+
+CREATE TABLE Books(book_id int AUTO_INCREMENT PRIMARY KEY , title varchar(130) not null, author_id FOREIGN KEY REFERENCES Authors(author_id), price not null, publication_date date);
+CREATE TABLE Authors(author_id int AUTO_INCREMENT PRIMARY KEY, author_name varchar(215) not null);
+CREATE TABLE Customers(customer_id int AUTO_INCREMENT PRIMARY KEY, customer_name varchar(215) not null, email varchar(215) not null UNIQUE, address TEXT);
+CREATE TABLE Orders(order_id int AUTO_INCREMENT PRIMARY KEY, customer_id FOREIGN KEY REFERENCES Customers(customer_id));
+CREATE TABLE Order_details(orderdetailid int primary key, order_id FOREIGN KEY REFERENCES Orders(order_id), book_id FOREIGN KEY REFERENCES Books(book_id), quantity double)
